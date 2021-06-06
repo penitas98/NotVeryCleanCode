46596369bc724bf1cb9ab084c6ae0605cd67d914
BlazejKrzyzanek <bl.krzyzanek@gmail.com>
2021-06-06 22:19:18 +0200

    Fill some values

--- src/main/java/domain/HashBatchRecordsBalance.java
+++ src/main/java/domain/HashBatchRecordsBalance.java
@@ -1,30 +1,67 @@
 package domain;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HashBatchRecordsBalance {
+
+    private BigDecimal hashTotalCredit;
+    private BigDecimal hashTotalDebit;
+    private Map<Integer, BatchTotal> batchTotals;
+    private BigDecimal totalFee;
+    private String collectionType;
+
+    public HashBatchRecordsBalance() {
+        hashTotalCredit = BigDecimal.ZERO;
+        hashTotalDebit = BigDecimal.ZERO;
+        batchTotals = new HashMap<>();
+        totalFee = BigDecimal.ZERO;
+        collectionType = "";
+    }
+
     public BigDecimal getHashTotalCredit() {
-        return null;
+        return hashTotalCredit;
+    }
+
+    public void setHashTotalCredit(BigDecimal hashTotalCredit) {
+        this.hashTotalCredit = hashTotalCredit;
     }
 
     public BigDecimal getHashTotalDebit() {
-        return null;
+        return hashTotalDebit;
+    }
+
+    public void setHashTotalDebit(BigDecimal hashTotalDebit) {
+        this.hashTotalDebit = hashTotalDebit;
+    }
+
+    public void setBatchTotals(Map<Integer, BatchTotal> batchTotals) {
+        this.batchTotals = batchTotals;
+    }
+
+    public void setTotalFee(BigDecimal totalFee) {
+        this.totalFee = totalFee;
+    }
+
+    public void setCollectionType(String collectionType) {
+        this.collectionType = collectionType;
     }
 
     public Map<Integer, BatchTotal> getBatchTotals() {
-        return null;
+        return batchTotals;
     }
 
-    public Integer getRecordsTotal() {
-        return null;
+    public Integer getRecordsTotal()
+    {
+        return batchTotals.size();
     }
 
     public BigDecimal getTotalFee() {
-        return null;
+        return totalFee;
     }
 
     public String getCollectionType() {
-        return null;
+        return collectionType;
     }
 }
